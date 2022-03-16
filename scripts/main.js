90ea1702b0f219a0e1e6f4b7726de3f38428c3ac
oracl12 <ostapko2003o@gmail.com>
2022-03-16 13:39:11 +0200

    as

--- scripts/main.js
+++ scripts/main.js
@@ -19,6 +19,6 @@ let isAlive=setInterval(
         let dinoTop=parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
         let cactusLeft=parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
         if (cactusLeft<50 && cactusLeft>0 && dinoTop>=140){
-            // alert("Game over")
+            alert("Game over")
         }
     },10)
